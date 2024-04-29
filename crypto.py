bd3b8caf60ee4e3d600b1cee80f6374fbe581dfc
ethanmcallister <a02264644@aggies.usu.edu>
2024-04-29 10:55:53 -0600

    added results.json file to show all results, changed txt file to be csv files.

--- crypto.py
+++ crypto.py
@@ -8,6 +8,7 @@ import json
 import requests
 from datetime import datetime
 import alpaca_trade_api as api
+import csv
 
 BASE_URL = "https://paper-api.alpaca.markets"
 KEY_ID = "PK914CBGHAJZBKQP8NMK"
@@ -20,6 +21,9 @@ def make_alpaca_orders(path_list):
     alpaca = api.REST(key_id=KEY_ID, secret_key=SECRET_KEY, base_url=BASE_URL)
 
     usd_symbols = []
+    
+    # create purchase history list to be returned
+    purchase_history = []
     
     # concatenate full symbol with USD at end
     for curr in path_list:
@@ -39,16 +43,20 @@ def make_alpaca_orders(path_list):
         # buy crypto
         order = alpaca.submit_order(symbol=symbol, qty=qty, side=side, type=type, time_in_force=time_in_force)
         print(f"buying {usd_symbols[i]}")
+        purchase_history.append(f"buying {usd_symbols[i]}")
 
         # sell cypto
         side = 'sell'
         order = alpaca.submit_order(symbol=symbol, qty=qty, side=side, type=type, time_in_force=time_in_force)
         print(f"selling {usd_symbols[i]}\n")
+        purchase_history.append(f"selling {usd_symbols[i]}")
 
     # account/account balance
     account = alpaca.get_account()
     account_cash = float(account.cash)
     print(f"\nBalance: {account_cash}")
+    
+    return purchase_history, account_cash
 
 
 def main():
@@ -96,8 +104,11 @@ def main():
     request = requests.get(url)  # request data from api
     exchange_rates_dict = json.loads(request.text)  # load json as string into python dictionary
 
+    # current directory
+    curr_dir = os.path.dirname(__file__)
+    
     # save path of json file
-    exchange_rates_json_file_path = "./data/json_data/exchange_rates.json"
+    exchange_rates_json_file_path = curr_dir + "/data/json_data/exchange_rates.json"
 
     # dump the dictionary into the json file
     with open(exchange_rates_json_file_path, "w") as json_file:
@@ -126,14 +137,13 @@ def main():
         # append the rate_row to the rates list
         rates.append(rate_row)
             
-    # current directory
-    curr_dir = os.path.dirname(__file__)
-    graph_visual_file = curr_dir + "\graph_visual.png"
-    edges_file = curr_dir + "\edges.txt"
+    graph_visual_file = curr_dir + "/graph_visual.png"
+    edges_file = curr_dir + "/edges.txt"
 
     # create graph object
     g = nx.DiGraph()
     edges = []
+    csv_data = []
     i = 0
 
     # go through currency combinations and append them to edges
@@ -144,16 +154,20 @@ def main():
                 edges.append((c1, c2, rates[i][j]))
                 # print("adding edge:", c1, c2, rates[i][j])
 
-                # write currency pair to text file (csv) with currency_pair_YYYY.MM.DD:HH.MM.txt as the file name
+                # write currency pair to csv with currency_pair_YYYY.MM.DD:HH.MM.txt as the file name
                 now = datetime.now()
-                name_1 = f"./data/currency_pair_data/currency_pair_"
-                name_2 = now.strftime("%Y.%m.%d-%H.%M.txt")
+                name_1 = curr_dir + "/data/currency_pair_data/currency_pair_"
+                name_2 = now.strftime("%Y.%m.%d-%H.%M.csv")
                 file_name = name_1 + name_2
-                text = f"{c1},{c2},{rates[i][j]},"
+                line = [c1, c2, rates[i][j]]
+                csv_data.append(line)
 
-                # write the currency pair data to the csv txt file
-                with open(file_name, "a") as csv_file:
-                    csv_file.write(text + "\n")
+    # write the currency pair data to the csv txt file
+    with open(file_name, "w", newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        
+        for row in csv_data:
+            writer.writerow(row)
 
     g.add_weighted_edges_from(edges)
     print()
@@ -170,14 +184,20 @@ def main():
     
     # create weight factor dictionary
     weight_factor_dict = {}
+    results_dict = {}
 
-    # iterate through all the possible combinations nodes 
+    # iterate through all the possible permutations nodes 
     for n1, n2 in permutations(g.nodes, 2):
         print("All paths from", n1, "to", n2, "-------------------------------------------------------------")
+        results_dict_key = f"{n1}-->{n2}"
+        results_dict[results_dict_key] = {}
+        
+        # keep track of path number
+        path_count = 1
 
         # for each path in all simple paths, calculate the weight to and from...
         for path in nx.all_simple_paths(g, source=n1, target=n2):
-            
+
             path_weight_to = 1.0
             # calculating the path weight from the first currency to the second
             for i in range(len(path) - 1):
@@ -185,7 +205,9 @@ def main():
 
             # print the path along with the weight
             print("Path to", path, path_weight_to)
-            # append path weight to to the list
+            
+            # append this path/weight tuple to the results dictionary
+            results_dict[results_dict_key][f"Path to ({path_count})"] = (path, path_weight_to)
 
             # before reversing, create a copy of the path list
             to_list = [] + path
@@ -200,6 +222,9 @@ def main():
 
             # print the path along with the weight
             print("Path from", path, path_weight_from)
+            
+            # append this path/weight tuple to the results dictionary
+            results_dict[results_dict_key][f"Path from ({path_count})"] = (path, path_weight_from)
 
             # calculate the weight factor of the to/from path and print it
             weight_factor = path_weight_to * path_weight_from
@@ -213,6 +238,9 @@ def main():
             key_tuple = tuple(map(tuple, key))
 
             weight_factor_dict[key_tuple] = weight_factor
+            
+            # increment path count
+            path_count += 1
     
         print("---------------------------------------------------------------------------------------------")
 
@@ -234,6 +262,14 @@ def main():
     # print the greathest paths weight factor
     print("Greatest Paths weight factor:", max_weight_factor_value)
     print("Paths:", list(max_weight_factor_key[0]), list(max_weight_factor_key[1]))
+    
+    # create results dictionary final results key
+    results_dict_results_key = "Final Results"
+    results_dict[results_dict_results_key] = {}
+    
+    # add smallest and greatest paths/weight factors to final results
+    results_dict[results_dict_results_key]["Smallest Path"] = (list(min_weight_factor_key[0]), list(min_weight_factor_key[1]), min_weight_factor_value)
+    results_dict[results_dict_results_key]["Greatest Path"] = (list(max_weight_factor_key[0]), list(max_weight_factor_key[1]), max_weight_factor_value)
 
     # iterate through list of max_weight_factor_key, see if each item is in currencies, if so set each item to be the value
     greatest_wf_path = [currencies[symbol] for symbol in list(max_weight_factor_key[0])]
@@ -241,7 +277,20 @@ def main():
     # if the max_weight_factor_value is greater than 1, make crypto orders with alpaca
     if max_weight_factor_value > 1:
         print()  # add space for terminal formatting
-        make_alpaca_orders(greatest_wf_path)
+        purchase_history_list, alpaca_balance = make_alpaca_orders(greatest_wf_path)
+    
+    # add purchase history and balance to results dictionary
+    purchases_key = "Purchases"
+    balance_key = "Balance"
+    results_dict[purchases_key] = purchase_history_list
+    results_dict[balance_key] = alpaca_balance
+    
+    # save path of json file
+    results_json_file_path = curr_dir + "/results.json"
+
+    # dump the dictionary into the json file
+    with open(results_json_file_path, "w") as json_file:
+        json.dump(results_dict, json_file) 
 
 
 main()
